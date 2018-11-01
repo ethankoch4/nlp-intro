3a202a4ae93faace897d91b7d2544d23c1b1008b
Ethan Koch <ethankoch4@gmail.com>
2018-11-01 13:53:03 -0600

    bow matrix

--- solutions/bow_representation.py
+++ solutions/bow_representation.py
@@ -1,5 +1,5 @@
 
-def get_bow_from_text(tokens, vocab):
+def get_bow_from_tokens(tokens, vocab):
     '''get BOW representation for text using vocab
     '''
     bow = {token: 0 for token in vocab}
@@ -22,3 +22,25 @@ def tokenize_text(text):
     tokenized = text.split()
     return tokenized
     
+def build_bow_matrix(bows, vocab):
+    '''build Bag-of-Words matrix using BOW dicts for each text
+    '''
+    
+    
+def texts_to_bow_matrix(texts):
+    '''from list of texts to a numpy array Bag-of-Words matrix
+    '''
+    vocab = build_vocab(texts)
+    bows = []
+    
+    for text in texts:
+        tokenized_text = tokenize_text(text)
+        bow = get_bow_from_tokens(tokenized_text)
+        bows.append(bow)
+    
+    bow_matrix = np.zeros((len(bows), len(vocab)))
+    for i, bow in enumerate(bows):
+        for j, token in enumerate(vocab):
+            if token in bow:
+                bow_matrix[i][j] = bow[token]
+    return bow_matrix
